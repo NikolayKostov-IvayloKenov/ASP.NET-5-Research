a1bdf6ec40856ba9645b9c8cfa8960d10b5670ea
Nikolay Kostov <admin@nikolay.it>
2014-11-21 18:33:18 +0200

    Removed unused packages from ASP.NET WebForms HTTP 2.0 demo

--- ASP.NET WebForms HTTP 2.0/App_Start/BundleConfig.cs
+++ ASP.NET WebForms HTTP 2.0/App_Start/BundleConfig.cs
@@ -28,19 +28,6 @@ namespace ASP.NET_WebForms_HTTP_2._0
                     "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
                     "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
                     "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
-
-            // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                            "~/Scripts/modernizr-*"));
-
-            ScriptManager.ScriptResourceMapping.AddDefinition(
-                "respond",
-                new ScriptResourceDefinition
-                {
-                    Path = "~/Scripts/respond.min.js",
-                    DebugPath = "~/Scripts/respond.js",
-                });
         }
     }
 }
