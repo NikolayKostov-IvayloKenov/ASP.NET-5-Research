7971d966db9dc2c1324a1bcd3d41b7f1feec03e3
Nikolay Kostov <admin@nikolay.it>
2014-11-21 19:34:50 +0200

    Demo for grunt and bower added

--- ASP.NET 5 Starter Web Application/gruntfile.js
+++ ASP.NET 5 Starter Web Application/gruntfile.js
@@ -1,4 +1,5 @@
-﻿// This file in the main entry point for defining grunt tasks and using grunt plugins.
+/// <binding AfterBuild='bower:less' ProjectOpened='bower:install' />
+// This file in the main entry point for defining grunt tasks and using grunt plugins.
 // Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 
 module.exports = function (grunt) {
@@ -11,6 +12,13 @@ module.exports = function (grunt) {
                     cleanTargetDir: false
                 }
             }
+        },
+        less: {
+            development: {
+                files: {
+                    "wwwroot/css/site.css": "wwwroot/css/site.less"
+                }
+            },
         }
     });
 
@@ -20,4 +28,5 @@ module.exports = function (grunt) {
     // The following line loads the grunt plugins.
     // This line needs to be at the end of this this file.
     grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-contrib-less");
 };
