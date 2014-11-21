268f25662b7bad2aaeb5333e9ca4770ea276794f
Nikolay Kostov <admin@nikolay.it>
2014-11-21 20:12:51 +0200

    Added simple demo for view components

---
+++ ASP.NET 5 Starter Web Application/ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using Microsoft.AspNet.Mvc;
+using System.Linq;
+
+namespace ASP.NET_5_Starter_Web_Application.ViewComponents
+{
+    public class SideBarViewComponent : ViewComponent
+    {
+        public SideBarViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke(int numbers)
+        {
+            var items = Enumerable.Range(1, numbers).ToList();
+
+            return View(items);
+        }
+    }
+}
