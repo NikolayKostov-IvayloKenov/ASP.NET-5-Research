d93892f223b15ae555657c54cc02b0c801f95434
Nikolay Kostov <admin@nikolay.it>
2014-11-21 16:54:26 +0200

    Implemented very basic http responder with ASP.NET 5

--- ASP.NET 5 Empty Application/Startup.cs
+++ ASP.NET 5 Empty Application/Startup.cs
@@ -8,7 +8,13 @@ namespace ASP.NET_5_Empty_Application
     {
         public void Configure(IApplicationBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/html";
+                context.Response.StatusCode = 200;
+
+                await context.Response.WriteAsync(string.Format("<h1>The time now is: {0}</h1>", DateTime.Now.ToString()));
+            });
         }
     }
 }
