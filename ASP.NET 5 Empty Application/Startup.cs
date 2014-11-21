87be3e060057659e05bee4f31a59f6b3e364fd07
Nikolay Kostov <admin@nikolay.it>
2014-11-21 17:10:12 +0200

    Demo for different target KRE versions added (for both ASP.NET 5 and ASP.NET Core 5)

--- ASP.NET 5 Empty Application/Startup.cs
+++ ASP.NET 5 Empty Application/Startup.cs
@@ -12,8 +12,15 @@ namespace ASP.NET_5_Empty_Application
             {
                 context.Response.ContentType = "text/html";
                 context.Response.StatusCode = 200;
-
-                await context.Response.WriteAsync(string.Format("<h1>The time now is: {0}</h1>", DateTime.Now.ToString()));
+#if ASPNET50
+                var frameworkInfo = string.Format("<h1>ASP.NET 5 on .NET Framework</h1>");
+                var timeInfo = string.Format("<h2>The time now is: {0}</h1>", NodaTime.SystemClock.Instance.Now);
+#elif ASPNETCORE50
+                var frameworkInfo = string.Format("<h1>ASP.NET Core on .NET Core</h1>");
+                var timeInfo = string.Format("<h2>The time now is: {0}</h1>", DateTime.Now.ToString());
+#endif
+                await context.Response.WriteAsync(frameworkInfo);
+                await context.Response.WriteAsync(timeInfo);
             });
         }
     }
